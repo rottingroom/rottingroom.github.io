d3e3fcae8b75b0196a08663cf0a0d25f4cb4d5aa
Anthony Raffini <traffini@gmail.com>
2016-03-10 16:33:40 -0800

    Media query transition, .spacer class,
    Had to remove most floats to accomplish media query transitions to
    containers.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var $root = $('html, body');
 $('a.smoothscroll').click(function() {
     $root.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top - 0
+        scrollTop: $( $.attr(this, 'href') ).offset().top - 60
     }, 500);
     return false;
 });
