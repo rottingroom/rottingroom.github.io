ee232d1205dacb2bef42de17b4b5bb0f19a2f360
Anthony Raffini <traffini@gmail.com>
2016-03-21 13:55:14 -0700

    moving folders

--- js/index.js
+++
@@ -1,7 +0,0 @@
-var $root = $('html, body');
-$('a.smoothscroll').click(function() {
-    $root.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top - 60
-    }, 500);
-    return false;
-});
